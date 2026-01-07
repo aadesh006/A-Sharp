a3089b8935185d38e56be1a5274e57c8ff98cbec
aadesh006 <aadeshchaudhari14@gmail.com>
2026-01-07 20:24:09 +0530

    Removed the debugging OP prints on terminal

--- common.h
+++ common.h
@@ -1,6 +1,6 @@
 #ifndef clox_common_h
 #define clox_common_h
-#define DEBUG_TRACE_EXECUTION
+//#define DEBUG_TRACE_EXECUTION //Prints OP_CMD on terminal for Debugging
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
