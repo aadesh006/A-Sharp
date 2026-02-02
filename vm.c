daac08ad532acf526e8d20dcc4ec3d931909833a
aadesh006 <aadeshchaudhari14@gmail.com>
2026-02-03 00:38:50 +0530

    Added the helper function for time Native Function - Fixed the compiler error in compilation

--- vm.c
+++ vm.c
@@ -37,6 +37,14 @@ static Value clockNative(int argCount, Value* args) {
   return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
 }
 
+static void defineNative(const char* name, NativeFn function) {
+  push(OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(OBJ_VAL(newNative(function)));
+  tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+  pop();
+  pop();
+}
+
 void initVM() {
   resetStack();
   vm.objects = NULL;
